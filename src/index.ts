232416b658a68e70138d9f43f918f40ab10f2586
Zoey <zoey@panties.moe>
2021-01-25 22:07:31 -0500

    Fixed keys.json path lol.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { TwitterClient } from './client/client';
 import { readFileSync } from 'fs';
 
-const keys = JSON.parse(readFileSync("./keys.json", { encoding: 'utf-8' }));
+const keys = JSON.parse(readFileSync(__dirname + "/keys.json", { encoding: 'utf-8' }));
 
 const client = new TwitterClient(keys);
